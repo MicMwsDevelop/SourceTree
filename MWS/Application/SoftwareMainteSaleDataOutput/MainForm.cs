998354c28456108ef987bd9f8c93fe7318397b76
勝呂 幹雄 <suguro@mic.jp>
2020-03-17 17:34:48 +0900

    Commit

---
+++ MWS/Application/SoftwareMainteSaleDataOutput/MainForm.cs
@@ -0,0 +1,93 @@
+﻿//
+// MainForm.cs
+// 
+// メイン画面クラス
+// 
+// Copyright (C) MIC All Rights Reserved.
+// 
+// Ver1.000 新規作成(2020/03/16 勝呂)
+//
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SoftwareMainteSaleDataOutput
+{
+	/// <summary>
+	/// メイン画面
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		/// <summary>
+		/// 環境設定
+		/// </summary>
+		private SoftwareMainteSaleDataOutputSettings Settings;
+
+		/// <summary>
+		/// デフォルトコンストラクタ
+		/// </summary>
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		/// <summary>
+		/// Form Load
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void MainForm_Load(object sender, EventArgs e)
+		{
+			Settings = SoftwareMainteSaleDataOutputSettingsIF.GetSettings();
+			textBoxFolder.Text = Settings.ExportDir;
+			textBoxFilename.Text = Settings.ExportFilename;
+		}
+
+		/// <summary>
+		/// 実行
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void buttonExec_Click(object sender, EventArgs e)
+		{
+			if (!Directory.Exists(textBoxFolder.Text))
+			{
+				MessageBox.Show("出力先が存在しません。", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				return;
+			}
+			Settings.ExportDir = textBoxFolder.Text;
+			Settings.ExportFilename = textBoxFilename.Text;
+			if (0 < Settings.Pathname.Length)
+			{
+				string msg = Program.OutputCsvFile(Settings.Pathname);
+				if (0 == msg.Length)
+				{
+					MessageBox.Show(string.Format("{0}を出力しました。", Settings.Pathname), "出力成功", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+				}
+				else
+				{
+					MessageBox.Show(msg, "エラー", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				}
+			}
+			else
+			{
+				MessageBox.Show("出力先が設定されていません。", "警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+			}
+		}
+
+		/// <summary>
+		/// Form Closing
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+		{
+			if (Directory.Exists(textBoxFolder.Text))
+			{
+				Settings.ExportDir = textBoxFolder.Text;
+			}
+			Settings.ExportFilename = textBoxFilename.Text;
+			SoftwareMainteSaleDataOutputSettingsIF.SetSettings(Settings);
+		}
+	}
+}
---
+++ MWS/Application/SoftwareMainteSaleDataOutput/Program.cs
@@ -0,0 +1,102 @@
+﻿//
+// Program.cs
+// 
+// ソフトウェア保守料売上データ出力 プログラムクラス
+// 
+// Copyright (C) MIC All Rights Reserved.
+// 
+// Ver1.000 新規作成(2020/03/16 勝呂)
+//
+using MwsLib.BaseFactory.SoftwareMainteSaleData;
+using MwsLib.Common;
+using MwsLib.DB.SqlServer.Junp;
+using MwsLib.DB.SqlServer.SoftwareMainteSaleData;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+
+namespace SoftwareMainteSaleDataOutput
+{
+	static class Program
+	{
+		/// <summary>
+		/// データベース接続先
+		/// </summary>
+		private const bool DATABASE_ACCESS_CT = false;
+
+		/// <summary>
+		/// アプリケーションのメイン エントリ ポイントです。
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			string[] cmds = Environment.GetCommandLineArgs();
+			if (2 <= cmds.Length)
+			{
+				if ("AUTO" == cmds[1].ToUpper())
+				{
+					SoftwareMainteSaleDataOutputSettings settings = SoftwareMainteSaleDataOutputSettingsIF.GetSettings();
+					OutputCsvFile(settings.Pathname);
+					return;
+				}
+			}
+			Application.Run(new MainForm());
+		}
+
+		/// <summary>
+		/// 売上データCSVファイルの出力
+		/// </summary>
+		/// <param name="pathname">出力ファイルパス名</param>
+		/// <returns>エラーメッセージ</returns>
+		public static string OutputCsvFile(string pathname)
+		{
+			try
+			{
+				List<CustomerUseInfoSoftwareMainte> list = SoftwareMainteSaleDataAccess.GetCustomerUseInfoSoftwareMainte1(Date.Today, DATABASE_ACCESS_CT);
+				if (0 < list.Count)
+				{
+					List<string> csvList = new List<string>();
+					int no = 60000;
+					Date lastMonth = Date.Today.LastDayOfLasMonth();
+					int taxRate = JunpDatabaseAccess.GetTaxRate(lastMonth, DATABASE_ACCESS_CT);
+					foreach (CustomerUseInfoSoftwareMainte data in list)
+					{
+						List<OrderSlipSoftwareMainte> order = SoftwareMainteSaleDataAccess.GetSoftwareMainteOrderSlip(data.CUSTOMER_ID, DATABASE_ACCESS_CT);
+						if (0 < order.Count)
+						{
+							csvList.Add(order[0].ToSale(no, taxRate, lastMonth));
+							no++;
+						}
+					}
+					if (0 < csvList.Count)
+					{
+						// 売上データ.txtの出力
+						using (var sw = new System.IO.StreamWriter(pathname, false))
+						{
+							foreach (string str in csvList)
+							{
+								sw.WriteLine(str);
+							}
+						}
+					}
+					else
+					{
+						return "ソフトウェア保守料売上対象なし";
+					}
+				}
+				else
+				{
+					return "ソフトウェア保守料売上対象なし";
+				}
+			}
+			catch (Exception ex)
+			{
+				return ex.Message;
+			}
+			return string.Empty;
+		}
+	}
+}
